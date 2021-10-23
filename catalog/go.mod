efbbed119d88c97eb5fb4e8ced30582834b25973
Denis <denis.junuzovic@fleetster.net>
2021-10-23 15:34:58 +0200

    mongo insert new catalog item

--- catalog/go.mod
+++ catalog/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gin-gonic/gin v1.7.4
 	github.com/google/uuid v1.3.0
 	github.com/streadway/amqp v1.0.0
+	go.mongodb.org/mongo-driver v1.7.3 // indirect
 )
--- catalog/main.go
+++ catalog/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"github.com/streadway/amqp"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"os"
 	"time"
@@ -16,11 +21,75 @@ type listing struct {
 }
 
 func main() {
+	msgs, mqConn, err := getMQConn()
+	if err != nil {
+		log.Fatal("error consuming queue messages:", err)
+	}
+
+	defer mqConn.Close()
+
+	client, cancel, err := getMongoConn()
+	if err != nil {
+		log.Fatalln("error connecting mongo client", err)
+	}
+	defer cancel()
+
+	item := new(listing)
+	forever := make(chan bool)
+	go func() {
+		for d := range msgs {
+			_ = json.Unmarshal(d.Body, &item)
+			log.Printf("New products event of type %s with id %s", d.RoutingKey, item.ProductID)
+			createListing(client, item)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for product events. To exit press CTRL+C")
+	<-forever
+}
+
+func createListing(client *mongo.Client, item *listing) {
+	collection := client.Database("catalog").Collection("listings")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+	res, err := collection.InsertOne(ctx, bson.M{
+		"productId": item.ProductID,
+		"name":      item.Name,
+		"desc":      item.Desc,
+		"imageUrl":  item.ImageUrl,
+	})
+	if err != nil {
+		log.Println("failed to insert item", err)
+	} else {
+		log.Println("created new catalog listing with", res.InsertedID.(primitive.ObjectID).Hex())
+	}
+}
+
+func getMongoConn() (*mongo.Client, context.CancelFunc, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://admin:admin123@"+os.Getenv("MONGO_URI")+"/catalog"))
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			log.Fatalln("error disconnecting mongo client", err)
+		}
+	}()
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalln("error pinging database", err)
+	}
+
+	log.Println("mongo connection successful!")
+
+	return client, cancel, err
+}
+
+func getMQConn() (<-chan amqp.Delivery, *amqp.Connection, error) {
 	conn, err := connect()
 	if err != nil {
 		log.Fatal("error getting connection", err)
 	}
-	defer conn.Close()
 
 	ch, err := conn.Channel()
 	if err != nil {
@@ -71,23 +140,7 @@ func main() {
 		false,
 		nil,
 	)
-
-	if err != nil {
-		log.Fatal("error consuming queue messages:", err)
-	}
-
-	listingItem := new(listing)
-	forever := make(chan bool)
-
-	go func() {
-		for d := range msgs {
-			_ = json.Unmarshal(d.Body, &listingItem)
-			log.Printf("New products event of type %s with id %s", d.RoutingKey, listingItem.ProductID)
-		}
-	}()
-
-	log.Printf(" [*] Waiting for product events. To exit press CTRL+C")
-	<-forever
+	return msgs, conn, err
 }
 
 func connect() (*amqp.Connection, error) {
